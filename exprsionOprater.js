e5054b4141ef16b0b932ed2952035f4d6eaf28b2
durganawale <nawledurga94@gmail.com>
2026-07-16 15:27:17 +0530

    first commit

---
+++ exprsionOprater.js
@@ -0,0 +1,163 @@
+//Arithmatic Operator
+
+let num1=10;
+let num2=5;
+
+console.log("Addition:",num1+num2);
+console.log("Substraction:", num1-num2);
+console.log("Multiplication:",num1*num2);
+console.log("Division:",num1/num2);
+console.log("Modulus:",num1 % num2);
+
+//Comparision Operator
+
+let a=10;
+let b=20;
+
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a==b);
+console.log(a===b);
+console.log(a !== b);
+
+//Combine Logical Oprator
+
+let isAdult=true;
+let hasLicense=false;
+
+console.log(isAdult && hasLicense);
+console.log(isAdult || hasLicense);
+console.log(! isAdult);
+
+// operator Precedence
+
+let result=10 + 2 * 5 - 8 / 4;
+console.log("result");
+
+//Assignment Operator
+let x=10;
+x += 5;
+console.log(x);
+x += 10;
+
+
+
+
+let First=10;
+let Second=30;
+
+console.log("Enter a first number:" ,First);
+console.log("Enter a first second:" ,Second);
+
+
+console.log("Addition:",First + Second );
+console.log("Substraction:",First - Second );
+console.log("Multiplicatin:",First * Second );
+console.log("Division:",First / Second );
+
+
+let Number1=10;
+let Number2=30;
+
+console.log("Enter a first number:" ,Number1);
+console.log("Enter a first second:" ,Number2);
+
+console.log("Is first number is greater than second",Number1 > Number2);
+console.log("Is first number is less than second",Number1 <  Number2);
+console.log("Is first number is equal to  second",Number1 == Number2);
+
+
+
+
+let Mathscore =93;
+let sciencescore = 89;
+
+console.log("Enter The Math Score:",Mathscore);
+console.log("Enter The Science score:",sciencescore);
+
+if(Mathscore<=90 && sciencescore <= 85)
+    {
+   console.log("Eligible for scolarship");
+ 
+}
+else{
+console.log("Not Eligible for Scolarship");
+}
+
+
+
+
+
+let A=10;
+
+console.log("Enter the initial valu of A",A);
+
+console.log("After adding  5:", A = A +5 );
+console.log("After Substract 5:" ,A= A-5);
+console.log("After Multiplied 5:" ,A= A*5);
+console.log("After Divide 5:" ,A= A/5);
+console.log("After Modulus 5:" ,A= A%5);
+
+let M= 10 + 3 * 5;
+let N= (10 + 3) * 5;
+let O= 20 / 2 + 3;
+let P= 20 / (2+ 3);
+
+console.log("M",M);
+console.log("N",N);
+console.log("O",O);
+console.log("P",P);
+
+let lenth=8;
+let width=6;
+let parameter ;
+let Area ;
+console.log("Enter The Length:",lenth);
+console.log("Enter The  Width:" ,width);
+
+console.log("Parameter:", 2 * lenth + width);
+
+console.log("Area:", lenth * width);
+
+
+let age=25;
+let Areyoucitizen;
+let Eligibletovoit;
+
+console.log("Enter The Age:",age);
+
+if (age<=18);
+{
+    console.log(Areyoucitizen && Eligibletovoit );
+}
+
+    console.log( ! Eligibletovoite);
+
+
+    let num=10;
+
+    if(num % 2){
+    console.log("even number",num);
+    }
+    console.log("Odd number",num);
+
+
+
+
+
+    let amount=120;
+    console.log("Enter total Amount:", amount);
+
+    if(amount<=$100){
+        console.log("apply 10 % discount", amount = 10 /100);
+    }
+
+
+
+
+
+
+
+
+
